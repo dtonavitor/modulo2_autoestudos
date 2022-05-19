8969fc3f361eca939170f5058cc667acb70fff6c
dtonavitor <vitor.barros@sou.inteli.edu.br>
2022-05-19 15:29:46 -0300

    adição Exercício de personalização da Etapa 4
    Adição de Jquery no currículo

---
+++ 03_AUT_EST_ENTREGA/Semana 4/frontend/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("ul").append(" <li>Curso Enterprise Design Thinking Practioner da IBM</li>.");
+    $("body").css({"font-family": "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"})
+    $("h1, h2, #main, #info, #profile").css({"text-align": "center"})
+    $("h3, p, li").css({"text-align": "left"})
+})
